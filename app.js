2a84d80f901e1dfd1d99957c3d65b6c94861850f
Joss Sparkes <joss.sparkes@gmail.com>
2022-03-30 17:39:49 +0100

    Removed x-powered-by from headers (#10)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+app.disable('x-powered-by');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
